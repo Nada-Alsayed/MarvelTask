6630361ada50d52524aec6edb9d73f10ccce900a
nada <nadahamed723@gmail.com>
2023-11-25 23:09:17 +0200

    files

--- Marvel/Extensions/HomeExtension.swift
+++ Marvel/Extensions/HomeExtension.swift
@@ -5,16 +5,7 @@
 //  Created by MAC on 24/11/2023.
 //
 
-//import Foundation
-//import UIKit
-//
-//extension HomeVC :UITableViewDelegate,UITableViewDataSource {
-//    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-//        <#code#>
-//    }
-//    
-//    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-//        <#code#>
-//    }
-//    
-//}
+import Foundation
+import UIKit
+
+
---
+++ Podfile
@@ -0,0 +1,14 @@
+# Uncomment the next line to define a global platform for your project
+  platform :ios, '12.0'
+
+target 'Marvel' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for Marvel
+ 	pod 'Kingfisher'
+    	pod 'Alamofire'
+    	pod 'lottie-ios'
+    	pod 'ReachabilitySwift'
+    	pod 'Cosmos'
+end
